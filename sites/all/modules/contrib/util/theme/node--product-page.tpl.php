f9a3c1ab2c81d9f191f41fc696de2466257dcdaa
dkjoseph <jgarrido@thisisdk.com>
2014-07-08 17:42:08 -0700

    uri test

--- sites/all/modules/contrib/util/theme/node--product-page.tpl.php
+++ sites/all/modules/contrib/util/theme/node--product-page.tpl.php
@@ -6,7 +6,7 @@
 <div class="col-sm-5 col-md-6">
 	<a href="" class="gal"><?php print render($content['product:field_image']); ?></a>
 	
-	<?php print $content['product:field_large_image']['#items'][0]['uri']; ?>
+	<?php print $content['product:field_image']['#items'][0]['uri']; ?>
 		
 </div>
 <div class="well col-sm-5 col-sm-offset-2 col-md-6 col-md-offset-0">
