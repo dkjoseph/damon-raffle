0ce3092106d25259fbf46fa160d71c5d575f2060
dkjoseph <jgarrido@thisisdk.com>
2014-07-21 20:25:24 -0700

    style

--- sites/all/modules/contrib/util/theme/node--product-page.tpl.php
+++ sites/all/modules/contrib/util/theme/node--product-page.tpl.php
@@ -11,7 +11,9 @@
 	<div class="block-views">
 	
 		<h3><?php print render($content['product:commerce_price']); ?></h3>		
-		<p style="font-weight: bold;font-size: 18px;"><?php print render($content['product:field_description']); ?></p>
+		<p style="font-weight: bold;font-size: 18px;">
+		 <?php print render($content['product:field_description']); ?>
+		</p>
 		<?php print render($content['field_product']); ?>
 
 	</div>
